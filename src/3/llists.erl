c510df3015f0cf268cff188a8f697a92139b496f
Vasiliy Ermolovich <younash@gmail.com>
2013-01-13 15:15:31 +0300

    add lists exercise

---
+++ src/3/llists.erl
@@ -0,0 +1,27 @@
+-module(llists).
+
+-export([
+          filter/2,
+          reverse/1,
+          concatenate/1
+        ]).
+
+% filter/2
+filter([], _) -> [];
+filter([Head | NewList], Num) when Head =< Num ->
+  [Head | filter(NewList, Num)];
+filter([Head | _], Num) when Head > Num -> [].
+
+% reverse/1
+reverse(List) -> reverse(List, []).
+
+% reverse/2
+reverse([Head | Tail], Result) -> reverse(Tail, [Head | Result]);
+reverse([], Result) -> Result.
+
+% concatenate/1
+concatenate(List) -> concatenate(List, []).
+
+% concatenate/2
+concatenate([], Result) -> Result;
+concatenate([Head | Tail], Result) -> Head ++ concatenate(Tail, Result).
---
+++ test/llists_tests.erl
@@ -0,0 +1,14 @@
+-module(llists_tests).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+llists_filter_test() ->
+  ?assert(llists:filter([1, 2, 3, 4, 5], 3) == [1, 2, 3]).
+
+llists_reverse_test() ->
+  ?assert(llists:reverse([1, 2, 3, 4, 5]) == [5, 4, 3, 2, 1]).
+
+llists_concatenate_test() ->
+  ?assert(llists:concatenate([[1, 2, 3], [], [4, five]]) == [1, 2, 3, 4, five]).
